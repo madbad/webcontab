969de4f18b35c7488333846d23eb306439a34576
unknown <Jonni@.(none)>
2011-07-27 22:41:35 +0200

    i dati del file dbf vengono suddivisi per camp ied inseriti in un array

--- dbf.php
+++ dbf.php
@@ -47,19 +47,54 @@ function echo_dbf($dbfname) {
         if (substr($buf,0,1)==chr(13)) {$goon=false;} // end of field list
         else {
             $field=unpack( "a11fieldname/A1fieldtype/Voffset/Cfieldlen/Cfielddec", substr($buf,0,18));
-            echo 'Field: '.json_encode($field).'<br/>';
+            //echo 'Field: '.json_encode($field).'<br/>';
             $unpackString.="A$field[fieldlen]$field[fieldname]/";
             array_push($fields, $field);}}
+	
     fseek($fdbf, $header['FirstRecord']+1); // move back to the start of the first record (after the field definitions)
     for ($i=1; $i<=$header['RecordCount']; $i++) {
         $buf = fread($fdbf,$header['RecordLength']);
         $record=unpack($unpackString,$buf);
         //echo 'record: '.json_encode($record).'<br/>';
-        echo $i.'----'.$buf.'<br/>';} //raw record
-    fclose($fdbf); }
+        echo $i.'----'.$buf.'<br/>';
+		$start=0;
+		
+
+		if($i==1){
+		echo '<table style="border:1px solid #000000"><tr>';
+			for ($h=0; $h<count($fields); $h++){
+				$field=$fields[$h];
+				echo '<td style="border:1px solid #000000">'.$field['fieldname']."</td>";
+			}
+		
+		}
+			if($i>33892 &&$i<33897){		
+			echo "</tr><tr>"; 	
+		}			
+		for ($h=0; $h<count($fields); $h++){
+			$field=$fields[$h];
+			//echo $field['fieldname'].':'.$field['fieldlen'].'<br>';
+			$name=$field['fieldname'];
+			$length=$field['fieldlen'];
+			$riga["$name"]=substr($buf,$start,$length);
+			$start+=$field['fieldlen'];
+
+			if($i>33892 &&$i<33897){
+						//echo $field['fieldname'].':"'.$riga["$name"].'"<br>';
+				echo '<td style="border:1px solid #000000">'.$riga["$name"]."</td>";
+			}
+			//ECHO substr($buf,$start,$field['fieldlen']);
+		}		
+
+		} //raw record
+
+    fclose($fdbf); 
+	return $riga;
+	}
 
 	
 echo '<pre>';
-echo_dbf($sFile);
+$riga=echo_dbf($sFile);
 echo '</pre>';
+echo '<br><br><br><br>Prova:<br>';
 ?>
