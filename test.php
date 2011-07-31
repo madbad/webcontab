ec4c0df915e743095bd52ce64cf53ef1d13df0df
unknown <Jonni@.(none)>
2011-07-31 11:18:50 +0200

    fffff

--- test.php
+++ test.php
@@ -12,8 +12,9 @@ Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\directory;Extended Properties=dB
 */
 
 
-
-$dsn = "Driver={Microsoft dBASE Driver (*.dbf)};SourceType=DBF;Dbq=C:\Programmi\EasyPHP-5.3.6.0\www\WebContab\FILEDBF;Exclusive=YES;collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;";
+//READONLY=true
+//DELETTED=1
+$dsn = "Driver={Microsoft dBASE Driver (*.dbf)};SourceType=DBF;DriverID=21;Dbq=C:\Programmi\EasyPHP-5.3.6.0\www\WebContab\FILEDBF\CONTAB;Exclusive=YES;collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;";
 $odbc=odbc_connect($dsn," "," ") or die('Could Not Connect to ODBC Database!');
 
 //questa funzionava
@@ -22,7 +23,11 @@ $odbc=odbc_connect($dsn," "," ") or die('Could Not Connect to ODBC Database!');
 //$strsql= "SELECT * FROM 03BORIGD.DBF WHERE f_numbol = '     795'";
 //$strsql= "SELECT * FROM 03BORIGD.DBF WHERE ((F_DATBOL >= CTOD('18/07/2011')) and (F_DATBOL <= CTOD('24/07/2011')))";
 //$strsql= "SELECT * FROM 03BORIGD.DBF WHERE month(F_DATBOL) between '12' and '12' AND year(F_DATBOL) between '2010' and '2010' AND day(F_DATBOL) between '10' and '10'";
-$strsql= "SELECT * FROM 03BORIGD.DBF WHERE month(F_DATBOL) between 12 and 12 AND year(F_DATBOL) between 2009 and 2010 AND day(F_DATBOL) between 9 and 10 ORDER BY F_NUMBOL, F_PROGRE ";
+//$strsql= "SELECT * FROM 03BORIGD.DBF WHERE month(F_DATBOL) between 12 and 12 AND year(F_DATBOL) between 2009 and 2010 AND day(F_DATBOL) between 9 and 10 ORDER BY F_NUMBOL, F_PROGRE ";
+
+$strsql= "SELECT * FROM 03BORIGD.DBF WHERE F_DATBOL >= '01/01/2010'  AND F_DATBOL < '01/02/2010'";
+
+
 echo $strsql;
 $query = odbc_exec($odbc, $strsql) or die (odbc_errormsg());
 
