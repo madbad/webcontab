0ce9cfb4ee293df4f5a79ceb1ec8123c4e0843a4
unknown <Jonni@.(none)>
2012-01-01 21:08:12 +0100

    list ddt test

--- my/js/apps.js
+++ my/js/apps.js
@@ -1,16 +1,51 @@
 function ddtWindow (){
+Ext.define('ddtList', {
+    extend: 'Ext.data.Model',
+    fields: [
+        { name: 'Numero', type: 'string' },
+        { name: 'Data', type: 'string' },
+        { name: 'Cliente', type: 'string' }
+    ]
+});
+
 	return Ext.create('Ext.window.Window', {
-		title: 'Hello',
+		title: 'Elenco DDT',
 		renderTo: document.body,
 		height: 200,
 		width: 400,
 		layout: 'fit',
-		items: {  // Let's put an empty grid in just to illustrate fit layout
+		items: {
 			xtype: 'grid',
+			id: 'ddtList',
 			border: false,
-			columns: [{header: 'World'},{header: 'test'}],                 // One header just for show. There's no data,
-			store: Ext.create('Ext.data.ArrayStore', {}) // A dummy empty data store
+			columns: [{header: 'Numero', dataIndex: 'Numero'}, //dataIndex serve per legare i campi della griglia ai dati nell'array
+			          {header: 'Data', dataIndex: 'Data'},
+					  {header: 'Cliente', dataIndex: 'Cliente'}],
+/*			
+			store: Ext.create('Ext.data.Store', {
+				model: 'ddtList',
+				data: [
+					{ 'Numero': '00001',    'Data': '16/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+					{ 'Numero': '00002',    'Data': '16/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+					{ 'Numero': '00003',    'Data': '17/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+					{ 'Numero': '00004',    'Data': '19/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+					{ 'Numero': '00005',    'Data': '19/12/2011', 'Cliente': 'Tesini e ghirlanda' }
+				]
+			})
+*/
+			store: Ext.create('Ext.data.Store', {
+				model: 'ddtList',
+				proxy: {
+					type: 'ajax',
+					url : './my/js/ddtList.json.php',
+					reader: {
+						type: 'json',
+						root: 'ddtList'
+					}
+				},
+				autoLoad: true
+			})
+			//grid.getSelectionModel().selectRow(0);
 		}
 	}).show()
 }
-
---
+++ my/js/ddtList.json.php
@@ -0,0 +1,10 @@
+{
+    success: true,
+    ddtList: [
+		{ 'Numero': '00001',    'Data': '16/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+		{ 'Numero': '00002',    'Data': '16/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+		{ 'Numero': '00003',    'Data': '17/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+		{ 'Numero': '00004',    'Data': '19/12/2011', 'Cliente': 'Tesini e ghirlanda' },
+		{ 'Numero': '00005',    'Data': '19/12/2011', 'Cliente': 'Tesini e ghirlanda' }
+    ]
+}
