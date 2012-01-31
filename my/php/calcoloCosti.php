c52cbd5c473242617626de6e6dfaf605f36af2ee
unknown <Jonni@.(none)>
2012-01-31 22:32:04 +0100

    extJS localization

--- my/php/calcoloCosti.php
+++ my/php/calcoloCosti.php
@@ -1,29 +1,25 @@
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="IT">
     <head>
         <title>WebContab Calcolo costi</title>
         <meta charset="utf-8">
-<style type="text/css" media="print" />      
- form{
-    display:none;
- }
-</style>
-         
+		
+		<!--ExtJs-->
+		<script src="./../js/ext.js/ext-all.js" type="text/javascript"></script>
+
+		<link href="./../js/ext.js/resources/css/ext-all.css" rel="stylesheet" type="text/css">
+		<script type="text/javascript">
+		Ext.require(['*']);
+		</script>
+ 		<script src="./../js/ext.js/locale/ext-lang-it.js" type="text/javascript"></script>
         <style type="text/css">
-@PAGE landscape {size: landscape;}
-TABLE {PAGE: landscape;}
-@page rotated { size : landscape }
-            body{
-             
-            //  column-count: 3;
-            //  -moz-column-count: 3;
-            //  -webkit-column-count: 3;
-            //  column-rule: 2px solid black;
-            //  -moz-column-rule: 2px solid black;
-            //  -webkit-column-rule: 2px solid black;
-            //  font-size:x-small;
+			@PAGE landscape {size: landscape;}
+			TABLE {PAGE: landscape;}
+			@page rotated { size : landscape }
+            .totali{
+                 font-size:1.5em;
             }
-            table, tr, td , th{
+			.righe table, .righe tr, .righe td, .righe th{
                 font-size:x-small;
                 padding:0px;
                 margin:0;
@@ -31,77 +27,152 @@ TABLE {PAGE: landscape;}
                 border:1px solid #000000;
                     border-collapse: collapse;
                 margin-left:0.5em;
-            }
-            td, th{
+			}
+            .righe td, .righe th{
                 padding-left:4px;
                 padding-right:4px;
             }
-            th{
+            .righe th{
                 font-weight:bold;
                 text-align:left;
             }
-            hr{
+            .righe hr{
                 margin-top:150px;
             }
-            #rimanenze td{
-                height:2em;
+            .rimanenze td{
+                height:3.5em;
                 width:9em;
                 text-align:left;
+				padding-left:1em;
+
             }
-            div {
+			.rimanenze table, .rimanenze tr, .rimanenze td, .rimanenze th{
+                font-size:x-small;
+                border:1px solid #000000;
+                border-collapse: collapse;
+								margin:1em;
+			}			
+            span div {
                 float:left;
-            }
-            form label{
-				display:block;
-				font-weight:bold;
-				width:15 em;
             }
             .totali{
                  font-size:1.5em;
             }
+			.tableContainer{
+				padding:0.2em;
+			}
+			h1{
+				font-size:2em;
+			}
         </style>
+		<style type="text/css" media="print" />      
+			.hideOnPrint{
+				display:none;
+			}
+			@PAGE landscape {size: landscape;}
+			TABLE {PAGE: landscape;}
+			@page rotated { size : landscape }
+		</style>		
     </head>
-  
-    <body>
-<?php
-$today = date("n-j-Y");
-if(@$_GET['startDate']){$startDate=$_GET['startDate'];}else{$startDate=$today;}
-if(@$_GET['endDate']){$endDate=$_GET['endDate'];}else{$endDate=$today;}
-  
-if(@$_GET['startDateR']){$startDateR=$_GET['startDateR'];}else{$startDateR=$today;}
-if(@$_GET['endDateR']){$endDateR=$_GET['endDateR'];}else{$endDateR=$today;}
-  
-?>
-<form name="input" action="./calcoloCosti.php?mode=print" method="get">
-    <input type="text" name="mode" value="print" style="display:none"/>
-    <label>Start date</label> <input type="text" name="startDate" value="<?php echo $startDate ?>"/>
-    <label>End date</label> <input type="text" name="endDate" value="<?php echo $endDate ?>"/>
-     
-    <label>Start date2</label> <input type="text" name="startDateR" value="<?php echo $startDateR ?>"/>
-    <label>End date2</label> <input type="text" name="endDateR" value="<?php echo $endDateR ?>"/>  
-    <label>Includi Radicchi</label> <input name="extraProducts" type="CHECKBOX" <?php if(@$_GET['extraProducts']) echo 'checked'  ?>/>
-    <button type="submit">Search</button>
-</form>
-  
+     <body>
+	<?php
+	$today = date("m-d-Y");
+	if(@$_POST['startDate']){$startDate=$_POST['startDate'];}else{$startDate=$today;}
+	if(@$_POST['endDate']){$endDate=$_POST['endDate'];}else{$endDate=$today;}
+	  
+	if(@$_POST['startDateR']){$startDateR=$_POST['startDateR'];}else{$startDateR=$today;}
+	if(@$_POST['endDateR']){$endDateR=$_POST['endDateR'];}else{$endDateR=$today;}
+	  
+	?>
+	
+	<span class="hideOnPrint" id="myForm">	
+	</span>
+	<script>
+		Ext.create('Ext.form.Panel', {
+			//renderTo: Ext.getBody(),
+			renderTo: document.getElementById('myForm'),
+			name: 'input',
+			method: 'POST',
+			layout: {
+    type: 'vbox',
+    align: 'right'
+},
+height:230,
+			url: './calcoloCosti.php',
+			standardSubmit: true,
+			bodyStyle: 'padding:10px',
+			title: 'Selezione parametri',
+			items: [{
+				xtype: 'hiddenfield',
+				name: 'mode',
+				value: 'print'
+			}, {
+				xtype: 'datefield',
+				format: 'm-d-Y',
+				anchor: '100%',
+				fieldLabel: 'From',
+				name: 'startDate',
+				value: '<?php echo $startDate ?>'  // defaults to today
+			}, {
+				xtype: 'datefield',
+				format: 'm-d-Y',
+				anchor: '100%',
+				fieldLabel: 'To',
+				name: 'endDate',
+				value: '<?php echo $endDate ?>' // defaults to today
+			}, {
+				xtype: 'datefield',
+				format: 'm-d-Y',
+				anchor: '100%',
+				fieldLabel: 'From (R)',
+				name: 'startDateR',
+				value: '<?php echo $startDateR ?>'  // defaults to today
+			}, {
+				xtype: 'datefield',
+				format: 'm-d-Y',
+				anchor: '100%',
+				fieldLabel: 'To (R)',
+				name: 'endDateR',
+				value: '<?php echo $endDateR ?>'  // defaults to today
+			}, {
+				xtype: 'checkboxfield',
+				boxLabel  : 'Includi radicchi',
+				name: 'extraProducts',
+				checked   : <?php if(@$_POST['extraProducts']) {echo 'true';}else{echo 'false';}  ?>,
+
+			}],
+			// Reset and Submit buttons
+			buttons: [{
+				text: 'Submit',
+				handler: function() {
+					var form = this.up('form').getForm();
+					form.submit();
+				}
+			}],
+		});
+	</script>
+
+<span>
+
 <?php
 require_once('./classes.php');
 
-if (@$_GET['mode']=='print'){
+if (@$_POST['mode']=='print'){
     //log start date for time execution calc
     $start = (float) array_sum(explode(' ',microtime()));
   
-    $table='<table id="rimanenze">';
-    $table.='<tr><td>Rimanenze</td><td></td></tr>';
+    $table='<table class="rimanenze">';
+    $table.='<tr><td colspan="2">Rimanenze</td></tr>';
+    $table.='<tr><td style="width:30%"></td><td></td></tr>';
     $table.='<tr><td></td><td></td></tr>';
     $table.='<tr><td></td><td></td></tr>';
     $table.='<tr><td></td><td></td></tr>';
     $table.='<tr><td></td><td></td></tr>';
-    $table.='<tr><td></td><td></td></tr>';
-    $table.='<tr><td>Tot.</td><td></td></tr>';
+    $table.='<tr><td>Tot.</td><td style="border:4px solid #000000"></td></tr>';
     $table.='</table>';
     $html='';
   
-    $html.="<div>";
+    $html.="<div class='tableContainer'>";
     $html.="<h1>Riccia</h1>";
 //riccia
 	// mercato
@@ -124,7 +195,7 @@ if (@$_GET['mode']=='print'){
     $html.=getArticleTable($params);					
     $html.=$table;
 //scarola  
-    $html.="</div><div>";
+    $html.="</div><div class='tableContainer'>";
     $html.="<h1>Scarola</h1>";
 	// mercato
 	$params = array("articles" => array('03'),
@@ -146,9 +217,9 @@ if (@$_GET['mode']=='print'){
 	$html.=getArticleTable($params);
     $html.=$table;
 
-    if(@$_GET['extraProducts']){
+    if(@$_POST['extraProducts']){
         //$html.='<div style="page-break-before: always"></div>';
-        $html.="</div><div>";
+		$html.="</div><div class='tableContainer'>";
         $html.="<h1>Tondo</h1>";
 //chioggia
 		// mercato
@@ -171,7 +242,7 @@ if (@$_GET['mode']=='print'){
 		$html.=getArticleTable($params);
         $html.=$table;
   
-        $html.="</div><div>";
+		$html.="</div><div class='tableContainer'>";
         $html.="<h1>Lungo</h1>";
 //treviso
 		// mercato
@@ -195,7 +266,7 @@ if (@$_GET['mode']=='print'){
         $html.=$table;
   
         //$html.='<div style="page-break-before: always"></div>';
-        $html.="</div><div>";
+		$html.="</div><div class='tableContainer'>";
         $html.="<h1>P.di Zucchero</h1>";
 //pan di zucchero
 		$params = array("articles" => array('31'),
@@ -215,7 +286,7 @@ if (@$_GET['mode']=='print'){
 						"costoCassa" => 0.70);
 		$html.=getArticleTable($params);
         $html.=$table;
-        $html.="</div><div>";
+		$html.="</div><div class='tableContainer'>";
         $html.="<h1>Semil.</h1>";
 //verona
 		// mercato
@@ -247,4 +318,5 @@ if (@$_GET['mode']=='print'){
     echo "<br>Exec time: ". sprintf("%.4f", ($end-$start))." seconds";
 }
 ?>
+</span>
 </body>
