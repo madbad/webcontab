79a881f4e883288f78a6d80acdd993a4ff8983ef
unknown <Jonni@.(none)>
2012-05-07 22:25:49 +0200

    UP

--- my/js/apps.js
+++ my/js/apps.js
@@ -1,18 +1,19 @@
 function ddtWindow (){
-Ext.define('ddtList', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name: 'numero', type: 'string' },
-        { name: 'data', type: 'string' },
-        { name: 'cliente', type: 'string' }
-    ]
-});
+	Ext.define('ddtList', {
+		extend: 'Ext.data.Model',
+		fields: [
+			{ name: 'numero', type: 'string' },
+			{ name: 'data', type: 'string' },
+			{ name: 'cliente', type: 'string' }
+		]
+	});
 	return Ext.create('Ext.window.Window', {
 		title: 'Elenco DDT',
 		renderTo: document.body,
-		height: 400,
+		height: 288,
 		width: 600,
 		layout: 'fit',
+		modal:true,
 		items: {
 			// other grid configurations goes here
 			xtype: 'grid',
@@ -28,53 +29,48 @@ Ext.define('ddtList', {
 				model: 'ddtList',
 				proxy: {
 					type: 'ajax',
-					//url : './my/js/ddtList.json.php',
-					url : './my/php/server.php',
+					url : './my/php/server.php?do=DdtList',
 					reader: {
 						type: 'json',
-						root: 'ddtList'
+						root: 'dataRoot'
 					}
 				},
 				autoLoad: true
 			}),
 			listeners: {
+				//activate:function(){
+				//},
 				render : function(grid){
-					grid.store.on('load', function(store, records, options){
-						//quando i dati sono stati caricati seleziono la prima colonna e gli assegno il focus
-						grid.getSelectionModel().select(0);
+					grid.store.on('datachanged', function(store, records, options){
+					
+
 						grid.getView().focus();
+						grid.getSelectionModel().select(0);
+
+						//quando i dati sono stati caricati seleziono la prima colonna e gli assegno il focus
+						//grid.ownerCt.focus();
+
+						
+						//grid.getView().focusRow(0);
+
+
 					  });
 					var map = new Ext.util.KeyMap(grid.getEl( ), {
 						key: Ext.EventObject.ENTER,
 						fn: function (){
 							//this return an array of the current selected(s) row(s)	
 							var selected=grid.getSelectionModel().getSelection( )[0];
-							alert('You have selected :'+selected.get('data')+' - '+selected.get('numero')+"1n We are now try to print it");
-							//send the print request
-							/*
-							Ext.Ajax.request({
-								url: './my/php/ddt.pdf.php',
-								params: {
-									numero: selected.get('numero'),
-									data: selected.get('data'),
-								},
-								success: function(response){
-									var text = response.responseText;
-									console.log(text+'**********');
-									//window.open('/pdfservlet');
-
-									// process server response here
-								}
-							});
-							*/
-							window.open('./my/php/ddt.pdf.php?data='+selected.get('data')+'&numero='+selected.get('numero'));
-
+							alert('You have selected :'+selected.get('data')+' - '+selected.get('numero'));
+							//ritorno i dati del ddt selezionato
+							this.result=selected;
+							this.ownerCt.result=selected;							
+							this.ownerCt.close();
 						},
 						scope: grid
 					});
 				},
 			}
-		}
+		},
 	}).show()
 }
 
