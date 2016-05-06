e41acab7b07e48b6f517fe42c4ee4c56de71c877
unknown <Utente@.(none)>
2016-05-06 17:16:08 +0200

    calcolo fatturato per cliente...

--- my/php/elencafatture2.php
+++ my/php/elencafatture2.php
@@ -14,66 +14,31 @@ set_time_limit ( 0);
 $test=new MyList(
 	array(
 		'_type'=>'Fattura',
-		'data'=>array('<>','01/12/2015','30/12/2015'),
-		'_autoExtend'=>'1',
-		//'_select'=>'numero,data,cod_cliente,tipo' //this was a try to optimize the select statement but gives no performance increase... It is even a little bit slower
-		//'cod_cliente'=>'SEVEN'
+		'data'=>array('<>','01/01/2015','31/12/2015'),
 	)
 );
 
 //ottengo una lista dei codici clienti 
 //(mi serve per creare una cache degli oggetti cliente in modo da non dover fare poi una query per ogni singolo oggetto cliente)
-$codiciCliente =array('=');
+$fatturato = array();
 $test->iterate(function($obj){
-	global $codiciCliente;
+	global $fatturato;
 	$codiceCliente=$obj->cod_cliente->getVal();
-	$codiciCliente[$codiceCliente]= $codiceCliente;
+	$fatturato[$codiceCliente] += $obj->getTotaleImponibile();
 });
+print_r($fatturato);
 
-//ricavo dalla lista precedente gli "oggetti" cliente
-$dbClienti=new MyList(
-	array(
-		'_type'=>'ClienteFornitore',
-		'codice'=>$codiciCliente,
-	)
+
+/*
+$params=array(
+	'data'=>'07/01/2016',
+	'numero'=>'1',
 );
+$fattura = new Fattura($params);
+echo $fattura->cod_cliente->getVal();
 
-function td($txt){
-	global $html;
-	$html.= '<td>'.$txt.'</td>';
-
-}
-//creo un nuovo array che ha per "indice" il codice cliente in modo da rendere più semplice ritrovare "l'oggetto" cliente
-$dbClientiWithIndex = array();
-$dbClienti->iterate(function($myCliente){
-	global $dbClientiWithIndex;
-	$codcliente = $myCliente->codice->getVal();
-	$dbClientiWithIndex[$codcliente]= $myCliente;
-});
-
-//stampo la lista delle fatture
-
-$html='<table class="borderTable spacedTable">';
-$test->iterate(function($obj){
-	global $html;
-	global $dbClientiWithIndex;
-	$dataInvioPec=$obj->__datainviopec->getVal();
-	
-	$tipo=$obj->tipo->getVal();
-	$cliente = $dbClientiWithIndex[$obj->cod_cliente->getVal()];
-
-	$html.= '<tr>';
-	td($tipo);
-	td($obj->numero->getVal());
-	td($obj->data->getFormatted());
-	td($obj->importo->getVal());
-	td($cliente->codice->getVal());
-	td($cliente->ragionesociale->getVal());
-	$html.= '</tr>';
-});
-
-$html.='</table>';
-echo $html;
+echo $fattura->getTotaleImponibile();
+*/
 ?>
 
 </body>
