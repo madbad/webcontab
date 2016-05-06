0ee366d6f0c10ff895cc8732557bcdcbbdaf2646
unknown <Utente@.(none)>
2016-05-06 17:16:44 +0200

    verifica di possibili errori nei prezzi

---
+++ my/php/verificaprezziseven.php
@@ -0,0 +1,46 @@
+
+<?php
+include ('./core/config.inc.php');
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>WebContab Calcolo costi</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="style_print.css" media="print">
+		</head>
+
+	<body>
+<?php
+
+//==============================================================================================================================
+
+//==============================================================================================================================
+$test=new MyList(
+		array(
+			'_type'=>'Riga',
+			'ddt_data'=>array('<>','01/01/16','31/12/16'),
+			//'cod_articolo'=>array('=','847'),
+			'cod_cliente'=>array('=','MARTI'),
+		)
+	);
+
+$prezzi=array();
+
+$test->iterate(function ($obj){
+	global $prezzi;
+	//$obj->cod_articolo->getVal();
+	//$obj->prezzo->getVal();
+	if ($prezzi[$obj->cod_articolo->getVal()] != $obj->prezzo->getVal()){
+		echo '<br> prezzo per ';
+		echo $obj->ddt_data->getFormatted();
+		echo ' prezzo per '.$obj->cod_articolo->getVal().' era '.$prezzi[$obj->cod_articolo->getVal()].' diventa '.$obj->prezzo->getVal();
+		$prezzi[$obj->cod_articolo->getVal()] = $obj->prezzo->getVal();
+	}
+});
+
+?>
+</body>
