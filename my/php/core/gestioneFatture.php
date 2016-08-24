cf53418b05fbd63df74a401f1aecc9caa637458c
unknown <Utente@.(none)>
2016-08-24 18:34:42 +0200

    Use sumatraPDF for printing

--- my/php/core/gestioneFatture.php
+++ my/php/core/gestioneFatture.php
@@ -79,46 +79,84 @@ if (@$_POST["do"]){
 			}
 			break;
 		case 'stampa':
-		//execInBackground('"C:\Programmi\Adobe\Reader 11.0\Reader\AcroRd32.exe"');
-		exec('start /b "C:\Programmi\Adobe\Reader 11.0\Reader\AcroRd32.exe" "C:\Programmi\EasyPHP-5.3.9\www\webcontab\my\php\core\stampe\myfile.pdf"');
-			//needed to prevent a bug flushing output to the broser
-			header( 'Content-type: text/html; charset=utf-8' );
-echo $_POST["fatture"];
-			$fatture = json_decode($_POST["fatture"]);
-print_r($fatture);
-			//mi preparo i parametri di ricerca della fattura
-			$count=0;
-			foreach ($fatture as $fattura){
-				$count++;
-				$params=array(
-					'numero' => $fattura->numero,
-					'data'   => $fattura->data,
-					'tipo'  => $fattura->tipo
-				);
-				//genero il mio oggetto fattura
-				$myFt= new Fattura($params);
-				$myFt->generaPdf();
-				$myFt->getPdfFileUrl();
-				
-				//linux only
-				//exec("lp file.pdf");
-				
-				//windows only
-				$acroreaderexe = '"C:\Programmi\Adobe\Reader 11.0\Reader\AcroRd32.exe"';
-				//$acroreaderexe = '"C:\Programmi\Foxit Software\Foxit Reader\FoxitReader.exe"';
-				$filename = '"'.$myFt->getPdfFileUrl().'"';
-				$printername = '"HP LaserJet M1530 MFP Series PCL 6"';
-				$drivername = '"Hp LaseJet M1530 MFP Series PCL 6"';
-				//$portname = '"IP_192.168.10.110"';
-				$portname = '"HPLaserJetM1536dnfMFP_copy_1"';
-				// acroreader.exe /t <filename> <printername> <drivername> <portname>
-				//"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" /h /t "C:\Folder\File.pdf" "Brother MFC-7820N USB Printer" "Brother MFC-7820N USB Printer" "IP_192.168.10.110"
-				$printCommand = $acroreaderexe.' /T '.$filename.' '.$printername.' '.$drivername.' '.$portname;
-				echo '<br>'.$printCommand;
-				//flush the output to the browser
-				flush();
-				ob_flush();
-				exec($printCommand);
+			if(false){//stampa usando adobe reader
+				exec('start /b "C:\Programmi\Adobe\Reader 11.0\Reader\AcroRd32.exe" "C:\Programmi\EasyPHP-5.3.9\www\webcontab\my\php\core\stampe\myfile.pdf"');
+				//needed to prevent a bug flushing output to the broser
+				header( 'Content-type: text/html; charset=utf-8' );
+				echo $_POST["fatture"];
+				$fatture = json_decode($_POST["fatture"]);
+				print_r($fatture);
+				//mi preparo i parametri di ricerca della fattura
+				$count=0;
+				foreach ($fatture as $fattura){
+					$count++;
+					$params=array(
+						'numero' => $fattura->numero,
+						'data'   => $fattura->data,
+						'tipo'  => $fattura->tipo
+					);
+					//genero il mio oggetto fattura
+					$myFt= new Fattura($params);
+					$myFt->generaPdf();
+					$myFt->getPdfFileUrl();
+					
+					//linux only
+					//exec("lp file.pdf");
+					
+					//windows only
+					$acroreaderexe = '"C:\Programmi\Adobe\Reader 11.0\Reader\AcroRd32.exe"';
+					//$acroreaderexe = '"C:\Programmi\Foxit Software\Foxit Reader\FoxitReader.exe"';
+					$filename = '"'.$myFt->getPdfFileUrl().'"';
+					$printername = '"HP LaserJet M1530 MFP Series PCL 6"';
+					$drivername = '"Hp LaseJet M1530 MFP Series PCL 6"';
+					//$portname = '"IP_192.168.10.110"';
+					$portname = '"HPLaserJetM1536dnfMFP_copy_1"';
+					// acroreader.exe /t <filename> <printername> <drivername> <portname>
+					//"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" /h /t "C:\Folder\File.pdf" "Brother MFC-7820N USB Printer" "Brother MFC-7820N USB Printer" "IP_192.168.10.110"
+					$printCommand = $acroreaderexe.' /T '.$filename.' '.$printername.' '.$drivername.' '.$portname;
+					echo '<br>'.$printCommand;
+					//flush the output to the browser
+					flush();
+					ob_flush();
+					exec($printCommand);
+				}
+			}else{//stampa usando sumatraPdf
+				//needed to prevent a bug flushing output to the broser
+				header( 'Content-type: text/html; charset=utf-8' );
+				echo $_POST["fatture"];
+				$fatture = json_decode($_POST["fatture"]);
+				print_r($fatture);
+				//mi preparo i parametri di ricerca della fattura
+				$count=0;
+				foreach ($fatture as $fattura){
+					$count++;
+					$params=array(
+						'numero' => $fattura->numero,
+						'data'   => $fattura->data,
+						'tipo'  => $fattura->tipo
+					);
+					//genero il mio oggetto fattura
+					$myFt= new Fattura($params);
+					$myFt->generaPdf();
+					$myFt->getPdfFileUrl();
+					
+					$sumatrapdfexe = 'C:\Programmi\SumatraPDF\SumatraPDF.exe';
+					$filename = '"'.$myFt->getPdfFileUrl().'"';
+					$printername = '"HP LaserJet M1530 MFP Series PCL 6"';
+					$printername = '"\\\\SERVER\PDFCreator"';
+					//$drivername = '"Hp LaseJet M1530 MFP Series PCL 6"';
+					//$portname = '"IP_192.168.10.110"';
+					//$portname = '"HPLaserJetM1536dnfMFP_copy_1"';
+					// acroreader.exe /t <filename> <printername> <drivername> <portname>
+					//"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" /h /t "C:\Folder\File.pdf" "Brother MFC-7820N USB Printer" "Brother MFC-7820N USB Printer" "IP_192.168.10.110"
+					//for command line options see: https://github.com/sumatrapdfreader/sumatrapdf/wiki/Command-line-arguments
+					$printCommand = $sumatrapdfexe.' -print-to '.$printername.' -print-settings "1x,fit" -silent -exit-when-done '.$filename;
+					echo '<br>'.$printCommand;
+					//flush the output to the browser
+					flush();
+					ob_flush();
+					exec($printCommand);
+				}
 			}
 		break;
 	}
