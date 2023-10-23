2a9e4907ae385d747f5fd612a27b2348634a83fa
madbad82 <madbad82@gmail.com>
2023-10-23 18:11:34 +0200

    up

--- my/php/utils/copy_all.bat
+++ my/php/utils/copy_all.bat
@@ -1,6 +1,6 @@
 @echo Copia dei file dbf e dbt in corso...
 :: la /d serve a copiare solo i file modificati
-xcopy  f:\CONTAB\*.* C:\Programmi\EasyPHP-5.3.9\www\WebContab\my\php\dati\FILEDBF\CONTAB\ /y /d
+xcopy  C:\CONTAB\*.* C:\Programmi\EasyPHP-5.3.9\www\WebContab\my\php\dati\FILEDBF\CONTAB\ /y /d
 @echo Finito!
 @echo Aggiorno il db sqlite
 :: C:\Programmi\"Mozilla Firefox"\firefox.exe -no-remote -p lafavorita http://localhost/webContab/my/php/aggiornadb.php
