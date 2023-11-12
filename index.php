be1a4743d3363af3d9cc6be5cc1b080a8c9519da
madbad82 <madbad82@gmail.com>
2023-11-12 15:52:09 +0100

    debug curl connections

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 phpinfo();
 
-
+error_reporting(-1); //0=spento || -1=acceso
   $ch = curl_init('https://www.howsmyssl.com/a/check'); 
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt ($ch, CURLOPT_CAINFO, "C:\Programmi\EasyPHP-5.3.9\www\webcontab\my\php\utils\cacert.pem");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  if(curl_exec($ch) === false)
+	{
+		echo 'Curl error: ' . curl_error($ch);
+	}
+	else
+	{
+		echo 'Operation completed without any errors';
+	}
+  
   $data = curl_exec($ch); 
   curl_close($ch); 
   $json = json_decode($data); 
---
+++ readme.txt
@@ -0,0 +1,9 @@
+************************************************************************************
+per le stampe usa sumatra pdf  ante 3.0.
+dalla versione 3.0 per qualche motivo stampa solo pagine bianche
+https://www.sumatrapdfreader.org/download-prev
+Version: 2.5.2
+Installer: https://www.sumatrapdfreader.org//dl//rel/2.5.2/SumatraPDF-2.5.2-install.exe
+************************************************************************************
+
+
